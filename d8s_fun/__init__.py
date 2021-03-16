6d7ab775e4a493b44bee25421b6fa79dc128662a
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-16 06:55:30 -0500

    Updating imports to reference new, d8s_... names

--- d8s_fun/__init__.py
+++ d8s_fun/__init__.py
@@ -6,10 +6,10 @@ except ImportError:
 from .fun import *
 
 try:
-    __version__ = version('democritus_fun')
+    __version__ = version('d8s_fun')
 except PackageNotFoundError:
     message = (
-        'Unable to find a version number for "democritus_fun". '
+        'Unable to find a version number for "d8s_fun". '
         + 'This likely means the library was not installed properly. '
         + 'Please re-install it and, if the problem persists, '
         + 'raise an issue here: https://github.com/democritus-project/democritus-fun/issues.'
